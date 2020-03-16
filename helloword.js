43bc9d0cc138f50909a3204b0746189d51a9e3a3
Nguyễn Đình Bảo <54382414+DB4111999@users.noreply.github.com>
2020-03-16 12:27:49 +0700

    Update helloword.js

--- helloword.js
+++ helloword.js
@@ -1,2 +1,8 @@
-var xinchao = "Hello Word";
-console.log(xinchao);
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('kết nối thành công');
+});
